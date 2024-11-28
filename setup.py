c83dab8130bc5f2c86cc5a25044a7ecbcb7b83ef
jererc <jererc@gmail.com>
2024-11-28 19:38:15 +0400

    do epic things

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webutils',
-    version='2024.11.27.182906',
+    version='2024.11.28.193812',
     author='jererc',
     author_email='jererc@gmail.com',
     url='https://github.com/jererc/webutils',
